03e36786c8cb3113a662b1a577862d89bc1d0268
Ignacio Pascual <ipascual@unexpectedit.com>
2017-02-17 17:49:46 -0500

    promises

--- products.js
+++ products.js
@@ -1,46 +1,59 @@
 var fs = require('fs');
+var q = require('q');
 var faker = require('faker')
 var parse = require('csv-parse');
 var stringify = require('csv-stringify');
 var csv = require('csv');
 var stringifier = csv.stringify({ header: true });
 
-var getHeaders = parse({columns: true, delimiter: ','}, function(err, data) {
-  return Object.keys(data).map(function (key) {
-    return record[key];
+(function () {
+  'use strict';
+
+  function getFakeProduct(data) {
+    data.sku = 'T-Shirt_' + faker.fake("{{address.city}}").replace(/\s/g, '') + '_' + faker.random.uuid();
+    data.price = Math.random() * (1000 - 10) + 10;
+    data.created_at = '';
+    data.updated_at = '';
+    data.qty = Math.random() * (1000 - 100) + 100;
+    data.visibility = 'Catalog, Search';
+    return data;
+  }
+
+  var stream = fs.createWriteStream(__dirname + '/products-generated.csv');
+  stream.once('open', function (fd) {
+    fs.readFile(__dirname + '/product-sample.csv', 'utf8', function (err, data) {
+
+      parse(data, {columns: true, delimiter: ',', from: 0, to: 1}, function (err, output) {
+        output[0] = getFakeProduct(output[0]);
+        csv.stringify(output, {header: true, delimiter: ','}, function (err, data) {
+          stream.write(data);
+
+          function loop(promise, fn) {
+            return promise.then(fn).then(function (wrapper) {
+              output[0] = getFakeProduct(output[0]);
+              csv.stringify(output, {header: false, delimiter: ','}, function (err, data) {
+                stream.write(data);
+              });
+              return !wrapper.done ? loop(q(wrapper.value), fn) : wrapper.value;
+            });
+          }
+
+          loop(q.resolve(1), function (i) {
+            console.log(i);
+            return {
+              done: i > 10,
+              value: ++i
+            };
+          }).catch(function (err) {
+            console.log(err);
+          }).
+          done(function () {
+            stream.end();
+          });
+
+        });
+      });
+    });
   });
-});
 
-var xxx = csv.transform(function(data) {
-  return Object.keys(record).map(function (key) {
-    return record[key];
-  });
-});
-
-var stringifyHeaders = stringify({header: true});
-
-var writeStream = fs.createWriteStream('file.csv');
-
-
-fs.createReadStream(__dirname+'/product-sample.csv').pipe(getHeaders).pipe(stringifyHeaders).pipe(process.stdout);
-
-
-
-  // csv.transform(data, function(data){
-  //   for(i=0;i<10;i++) {
-  //     data.sku = 'T-Shirt_' + faker.fake("{{address.city}}").replace(/\s/g, '') + '_' + faker.random.uuid();
-  //     data.price = Math.random() * (1000 - 10) + 10;
-  //     data.created_at = '';
-  //     data.updated_at = '';
-  //     data.qty = Math.random() * (1000 - 100) + 100;
-  //     data.visibility = 'Catalog, Search';
-  //     return data;
-  //   }
-  // }, function(err, data){
-  //   csv.stringify(data, function(err, data){
-  //     console.log(err);
-  //     process.stdout.write(data);
-  //   });
-  // });
-// });
-
+})();
